4253bd9fa78fb55ab3e344aff3115217c9faf922
David Jensen <david_jensen3@hotmail.com>
2015-03-12 18:10:28 +0100

    pragma once test separate

---
+++ Tests/ContainsPragmaOnce.cs
@@ -0,0 +1,26 @@
+﻿using Cycles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class ContainsPragmaOnce
+    {
+        String[] testfiles = {"test","enum","mess","main","classes","structs",
+        "keywords","temlpates","bracemacro","include","sample"};
+        
+        [TestMethod]
+        public void testContainsPragmaOnce()
+        {
+            foreach(String testname in testfiles){
+                UhsTest test = new UhsTest(testname);
+                test.parse();
+                if(!test.newHeaderContents.Contains("#pragma once"))
+                {
+                    System.IO.File.WriteAllText(test.header, "#pragma once\r\n");
+                    throw new FormatException("Needs to contain pragma once before ran");
+                }
+            }
+        }
+    }
