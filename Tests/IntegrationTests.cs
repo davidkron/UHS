f705bfa8d4864cf17a499c3f3c286a0e4328a2cd
davidkron <david_jensen3@hotmail.com>
2015-03-19 17:17:39 +0100

    Renamed unittests to integration, and movefunctionbody to unit

---
+++ Tests/IntegrationTests.cs
@@ -0,0 +1,94 @@
+﻿using Cycles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestingFramework.test("test");
+            
+        }
+
+        [TestMethod]
+        public void TestEnum()
+        {
+            TestingFramework.test("enum");
+        }
+
+        [TestMethod]
+        public void TestMess()
+        {
+            TestingFramework.test("mess");
+        }
+
+        [TestMethod]
+        public void NamespaceTest()
+        {
+            TestingFramework.test("namespace");
+        }
+
+        [TestMethod]
+        public void MainTest()
+        {
+            TestingFramework.test("main");
+        }
+
+        [TestMethod]
+        public void Classtest()
+        {
+            TestingFramework.test("classes");
+        }
+
+        [TestMethod]
+        public void Structs()
+        {
+            TestingFramework.test("structs");
+        }
+
+        [TestMethod]
+        public void Keywords()
+        {
+            TestingFramework.test("keywords");
+        }
+
+        [TestMethod]
+        public void Templates()
+        {
+            TestingFramework.test("templates");
+        }
+
+        [TestMethod]
+        public void BraceMacro()
+        {
+            TestingFramework.test("bracemacro");
+        }
+
+        [TestMethod]
+        public void TestInclude()
+        {
+            TestingFramework.test("include");
+        }
+
+        [TestMethod]
+        public void StressTest()
+        {
+            for(int i = 0; i < 5; i++) { 
+                TestingFramework.test("test");
+                Thread.Sleep(10);
+            }
+        }
+
+        [TestMethod]
+        public void TestSample()
+        {
+            TestingFramework.test("sample");
+        }
+    }
+}
--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,94 +1,28 @@
-﻿using Cycles;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Threading;
+using EnvDTE;
+using Cycles.Converting;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.VCCodeModel;
 
 namespace Tests
 {
-    [TestClass]
-    public class UnitTests
-    {
+	[TestClass]
+	public class UnitTests
+	{
+		[TestMethod]
+		public void TestMoveFunctionBody()
+		{
+			string headerContent = null, sourceContent = null;
+			String uhsContent = @"
+int main()
+{
+	return 5;
+}";
+			TestingFramework.testString(uhsContent,out headerContent, out sourceContent);
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            TestingFramework.test("test");
-            
+			StringAssert.Contains(sourceContent, "return 5");
+			StringAssert.Contains(headerContent, "main();");
         }
-
-        [TestMethod]
-        public void TestEnum()
-        {
-            TestingFramework.test("enum");
-        }
-
-        [TestMethod]
-        public void TestMess()
-        {
-            TestingFramework.test("mess");
-        }
-
-        [TestMethod]
-        public void NamespaceTest()
-        {
-            TestingFramework.test("namespace");
-        }
-
-        [TestMethod]
-        public void MainTest()
-        {
-            TestingFramework.test("main");
-        }
-
-        [TestMethod]
-        public void Classtest()
-        {
-            TestingFramework.test("classes");
-        }
-
-        [TestMethod]
-        public void Structs()
-        {
-            TestingFramework.test("structs");
-        }
-
-        [TestMethod]
-        public void Keywords()
-        {
-            TestingFramework.test("keywords");
-        }
-
-        [TestMethod]
-        public void Templates()
-        {
-            TestingFramework.test("templates");
-        }
-
-        [TestMethod]
-        public void BraceMacro()
-        {
-            TestingFramework.test("bracemacro");
-        }
-
-        [TestMethod]
-        public void TestInclude()
-        {
-            TestingFramework.test("include");
-        }
-
-        [TestMethod]
-        public void StressTest()
-        {
-            for(int i = 0; i < 5; i++) { 
-                TestingFramework.test("test");
-                Thread.Sleep(10);
-            }
-        }
-
-        [TestMethod]
-        public void TestSample()
-        {
-            TestingFramework.test("sample");
-        }
-    }
+	}
 }
